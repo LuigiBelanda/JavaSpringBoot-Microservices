147ee8abc352fa25f0cc53f58b4c2f9748ca08e4
Luigi Belanda <luigi.belanda.milani@gmail.com>
2023-05-29 16:11:45 -0300

    Step 08 - Implementing GET Methods for User Resource

--- restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/user/UserDaoService.java
+++ restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/user/UserDaoService.java
@@ -1,27 +1,32 @@
 /*
-A classe `UserDaoService` é um componente Spring Boot que atua como uma camada de
-acesso a dados simulada para operações relacionadas a usuários. Vamos analisar o
-código e explicar suas funcionalidades:
+A classe `UserDaoService` é um componente do Spring Boot que simula uma camada de acesso
+a dados para operações relacionadas a usuários. Vamos analisar o código e explicar suas funcionalidades:
 
 1. A anotação `@Component` é usada para marcar a classe como um componente gerenciado
-pelo Spring. Isso significa que o Spring irá gerenciar a criação e injeção de
-dependências dessa classe.
+pelo Spring. Isso permite que a classe seja detectada e utilizada pelo mecanismo de
+injeção de dependência do Spring.
 
-2. A classe `UserDaoService` possui um campo estático `users`, que é uma lista de
-objetos `User`. Essa lista é usada para armazenar os usuários simulados.
+2. A classe possui um campo estático chamado `users`, que é uma lista de objetos `User`.
+Essa lista é usada para armazenar os usuários simulados.
 
-3. No bloco estático inicializador, são criados alguns objetos `User` e adicionados
-à lista `users`. Esses usuários simulados possuem um ID, um nome e uma data de
-nascimento que é calculada subtraindo-se uma determinada quantidade de anos da data atual.
+3. No bloco estático inicializador, são criados alguns objetos `User` com IDs, nomes e
+ datas de nascimento simulados. Esses usuários são adicionados à lista `users`. A data
+ de nascimento é calculada subtraindo uma determinada quantidade de anos da data atual,
+ usando o método `LocalDate.now().minusYears()`.
 
-4. O método `findAll()` retorna a lista de usuários armazenada no `UserDaoService`.
-Esse método é responsável por recuperar todos os usuários existentes.
+4. O método `findAll()` retorna a lista de usuários armazenada no `UserDaoService`. Esse
+método é responsável por recuperar todos os usuários existentes.
 
-Resumindo, a classe `UserDaoService` simula um serviço de acesso a dados para
-operações relacionadas a usuários. Ela mantém uma lista de usuários simulados
-e fornece um método para recuperar todos os usuários. Essa classe é marcada
-como um componente do Spring para que possa ser gerenciada e injetada em outras
-partes da aplicação, se necessário.
+5. O método `findOne(int id)` recebe um ID como parâmetro e retorna o usuário correspondente
+a esse ID. Ele utiliza o método `findFirst()` para encontrar o primeiro usuário na lista
+`users` que corresponda ao ID fornecido. A expressão lambda `user -> user.getId().equals(id)`
+é usada para verificar se o ID do usuário é igual ao ID fornecido.
+
+Resumindo, a classe `UserDaoService` simula uma camada de acesso a dados para operações
+relacionadas a usuários. Ela mantém uma lista de usuários simulados e fornece métodos
+para recuperar todos os usuários e encontrar um usuário específico com base no ID.
+Essa classe é marcada como um componente do Spring para que possa ser gerenciada e
+injetada em outras partes da aplicação, se necessário.
 */
 
 package com.rest.webservices.restfulwebservices.user;
@@ -31,6 +36,7 @@ import org.springframework.stereotype.Component;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 @Component
 public class UserDaoService {
@@ -44,5 +50,10 @@ public class UserDaoService {
 
     public List<User> findAll() {
         return users;
+    }
+
+    public User findOne(int id) {
+        Predicate<? super User> predicate = user -> user.getId().equals(id);
+        return users.stream().filter(predicate).findFirst().get();
     }
 }
---
+++ restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/user/UserResource.java
@@ -0,0 +1,57 @@
+/*
+O código fornecido é uma classe chamada `UserResource` que atua como um controlador REST
+para a entidade `User`. Vamos analisar o código e explicar suas funcionalidades:
+
+1. A classe `UserResource` é anotada com `@RestController`, indicando que é um controlador
+REST que trata solicitações HTTP.
+
+2. A classe possui um campo `service` do tipo `UserDaoService`, que é injetado por meio do
+construtor. Isso permite que o controlador utilize os serviços fornecidos pela classe
+`UserDaoService` para acessar e manipular os dados dos usuários.
+
+3. O método `retrieveAllUsers()` é mapeado para a URL `/users` e será invocado quando uma
+solicitação HTTP GET for feita para essa URL. Ele chama o método `findAll()` do serviço
+`UserDaoService` para obter uma lista de todos os usuários e retorna essa lista como resposta HTTP.
+
+4. O método `retrieveUser(@PathVariable int id)` é mapeado para a URL `/users/{id}` e será
+invocado quando uma solicitação HTTP GET for feita para essa URL com um valor no espaço
+reservado `{id}`. Ele chama o método `findOne(id)` do serviço `UserDaoService` para obter
+o usuário com o ID fornecido e retorna esse usuário como resposta HTTP.
+
+5. A anotação `@PathVariable` é usada para indicar que o valor na URL é capturado e
+atribuído ao parâmetro `id` do método.
+
+Resumindo, a classe `UserResource` é um controlador REST que trata as solicitações
+HTTP relacionadas aos usuários. Ela utiliza o serviço `UserDaoService` para acessar
+e manipular os dados dos usuários. Os métodos mapeados respondem a solicitações GET
+em URLs específicas, retornando listas de usuários ou usuários individuais, dependendo do caso.
+*/
+
+package com.rest.webservices.restfulwebservices.user;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserResource {
+    private UserDaoService service;
+
+    public UserResource(UserDaoService service) {
+        this.service = service;
+    }
+
+    // Get /users
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers() {
+        return service.findAll();
+    }
+
+    // Get /users/pathVariable
+    @GetMapping("/users/{id}")
+    public User retrieveUser(@PathVariable int id) {
+        return service.findOne(id);
+    }
+}

4e75bf31ee1c1589160ac7aed10b323e4f7830ee
Luigi Belanda <luigi.belanda.milani@gmail.com>
2023-05-29 16:35:28 -0300

    Step 10 - Enhancing POST Method to return correct HTTP Status Code and Location

--- restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/user/UserResource.java
+++ restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/user/UserResource.java
@@ -29,8 +29,11 @@ em URLs específicas, retornando listas de usuários ou usuários individuais, d
 
 package com.rest.webservices.restfulwebservices.user;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -53,8 +56,50 @@ public class UserResource {
         return service.findOne(id);
     }
 
+    /*
+    O trecho de código fornecido é um método chamado `createUser` em um controlador REST
+    que trata solicitações HTTP POST para criar um novo usuário. Vamos analisar o código
+     e explicar suas funcionalidades:
+
+    1. A anotação `@PostMapping("/users")` é usada para mapear o método para a URL
+    "/users" e especificar que ele deve ser invocado quando uma solicitação
+    HTTP POST for feita para essa URL.
+
+    2. O parâmetro `@RequestBody User user` indica que o corpo da solicitação HTTP
+    deve ser convertido em um objeto do tipo `User`. O Spring irá realizar
+    automaticamente essa conversão, baseado na estrutura e conteúdo do corpo da solicitação.
+
+    3. O método `service.save(user)` é chamado para salvar o objeto `user` recebido
+    no banco de dados ou em algum outro local de armazenamento.
+    O detalhe da implementação do método `save` não é fornecido,
+    mas geralmente ele seria responsável por persistir o objeto `user` e
+    retornar o usuário salvo com um ID atribuído.
+
+    4. A variável `location` é criada usando a classe `ServletUriComponentsBuilder`
+    para construir a URI de resposta que aponta para o recurso recém-criado.
+    A URI é construída a partir da URL atual da solicitação (`fromCurrentRequest()`),
+    acrescentando o ID do usuário salvo na URL (`path("/{id}").buildAndExpand(savedUser.getId())`).
+
+    5. A resposta é construída usando a classe `ResponseEntity`.
+    O método `created(location)` é chamado para criar uma resposta com o
+    status HTTP 201 (Created) e o cabeçalho `Location` definido com a URI do
+    recurso criado. Em seguida, `build()` é chamado para criar a resposta final
+    a ser enviada ao cliente.
+
+    Resumindo, o método `createUser` é responsável por tratar solicitações
+    HTTP POST para criar um novo usuário. Ele recebe um objeto `User` no
+    corpo da solicitação, salva o usuário no banco de dados ou em algum
+    outro local de armazenamento, e retorna uma resposta HTTP com o status
+    201 (Created) e o cabeçalho `Location` apontando para a URI do usuário recém-criado.
+    */
     @PostMapping("/users")
-    public void createUser(@RequestBody User user) {
-        service.save(user);
+    public ResponseEntity<Object> createUser(@RequestBody User user) {
+        User savedUser = service.save(user);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                            .path("/{id}")
+                            .buildAndExpand(savedUser.getId())
+                            .toUri();
+        return ResponseEntity.created(location).build();
     }
 }

cdd915de4b0360b04ebf0a26cd148f31f1d66cf0
Luigi Belanda <luigi.belanda.milani@gmail.com>
2023-05-30 15:55:30 -0300

    Step 24 - Implementing Dynamic Filtering for REST API

--- restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/filtering/FilteringController.java
+++ restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/filtering/FilteringController.java
@@ -1,5 +1,41 @@
+/*
+Este é um exemplo de um controlador em um aplicativo de serviço da web RESTful que demonstra
+o uso de filtragem de propriedades com a biblioteca Jackson em um endpoint que retorna dados JSON.
+
+No controlador `FilteringController`, existem dois métodos de endpoint mapeados com as
+anotações `@GetMapping`. Vamos analisar cada um deles:
+
+1. Método `filtering()`:
+   - Este método responde ao endpoint "/filtering".
+   - Cria uma instância da classe `SomeBean` com os valores "value1", "value2" e "value3".
+   - Em seguida, cria um objeto `MappingJacksonValue` e o inicializa com a instância `someBean`.
+   - Define um filtro `SimpleBeanPropertyFilter` para filtrar todas as propriedades, exceto "filed1" e "field3".
+   - Cria um provedor de filtros `SimpleFilterProvider` e adiciona o filtro com o nome "SomeBeanFilter".
+   - Define os filtros no objeto `MappingJacksonValue`.
+   - Retorna o objeto `MappingJacksonValue`, que será convertido em JSON e enviado como resposta.
+
+2. Método `filteringList()`:
+   - Este método responde ao endpoint "/filtering-list".
+   - Cria uma lista de instâncias da classe `SomeBean` com os valores "value4", "value5" e "value6".
+   - Em seguida, cria um objeto `MappingJacksonValue` e o inicializa com a lista.
+   - Define um filtro `SimpleBeanPropertyFilter` para filtrar todas as propriedades, exceto "filed2" e "field3".
+   - Cria um provedor de filtros `SimpleFilterProvider` e adiciona o filtro com o nome "SomeBeanFilter".
+   - Define os filtros no objeto `MappingJacksonValue`.
+   - Retorna o objeto `MappingJacksonValue`, que será convertido em JSON e enviado como resposta.
+
+No geral, esse controlador usa o `MappingJacksonValue` em combinação com
+`SimpleBeanPropertyFilter` e `SimpleFilterProvider` para filtrar quais propriedades de um
+objeto ou lista são incluídas na resposta JSON. Os filtros são adicionados usando um nome de
+filtro personalizado ("SomeBeanFilter" neste caso) para que possam ser aplicados aos objetos
+`MappingJacksonValue`.
+*/
+
 package com.rest.webservices.restfulwebservices.filtering;
 
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,12 +45,26 @@ import java.util.List;
 @RestController
 public class FilteringController {
     @GetMapping("/filtering")
-    public SomeBean filtering() {
-        return new SomeBean("value1", "value2", "value3");
+    public MappingJacksonValue filtering() {
+        SomeBean someBean = new SomeBean("value1", "value2", "value3");
+
+        MappingJacksonValue mappingJacksonValue =new MappingJacksonValue(someBean);
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("filed1", "field3");
+        FilterProvider filters = new SimpleFilterProvider().addFilter("SomeBeanFilter", filter);
+        mappingJacksonValue.setFilters(filters);
+
+        return mappingJacksonValue;
     }
 
     @GetMapping("/filtering-list")
-    public List<SomeBean> filteringList() {
-        return Arrays.asList(new SomeBean("value4", "value5", "value6"));
+    public MappingJacksonValue filteringList() {
+        List<SomeBean> list = Arrays.asList(new SomeBean("value4", "value5", "value6"));
+
+        MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(list);
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("filed2", "field3");
+        FilterProvider filters = new SimpleFilterProvider().addFilter("SomeBeanFilter", filter);
+        mappingJacksonValue.setFilters(filters);
+
+        return mappingJacksonValue;
     }
 }
--- restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/filtering/SomeBean.java
+++ restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/filtering/SomeBean.java
@@ -15,17 +15,35 @@ públicas e privadas são consideradas, a menos que sejam especificamente ignora
 annotations `@JsonIgnore` ou `@JsonIgnoreProperties`. Essas annotations são úteis quando você
 deseja excluir determinadas propriedades de um objeto Java da representação JSON, como quando a
 propriedade contém informações sensíveis ou não é relevante para o contexto de serialização/desserialização.
+
+A annotation @JsonFilter é fornecida pela biblioteca Jackson e permite especificar um filtro
+personalizado para ser usado durante a serialização de objetos em JSON. O valor atribuído a
+@JsonFilter é um nome de filtro personalizado, neste caso, "SomeBeanFilter".
+
+Essa annotation indica que a classe SomeBean deve ser filtrada usando o filtro chamado
+"SomeBeanFilter". O filtro é configurado no controlador (como visto no exemplo anterior)
+usando SimpleFilterProvider e associado ao nome do filtro "SomeBeanFilter".
+O objetivo dessa configuração é permitir que o filtro seja aplicado seletivamente às
+propriedades da classe SomeBean durante a serialização em JSON. Nesse caso, o filtro
+está sendo usado para filtrar quais propriedades são incluídas na resposta JSON com base em seus nomes.
+
+Em resumo, com a adição da annotation @JsonFilter na classe SomeBean e a
+configuração correspondente no controlador, é possível aplicar filtros
+personalizados durante a serialização de objetos JSON, permitindo maior
+flexibilidade no controle das propriedades que serão incluídas na resposta JSON.
 */
 
 package com.rest.webservices.restfulwebservices.filtering;
 
+import com.fasterxml.jackson.annotation.JsonFilter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 // @JsonIgnoreProperties("field1")
+@JsonFilter("SomeBeanFilter")
 public class SomeBean {
     private String field1;
-    @JsonIgnore
+    // @JsonIgnore
     private String field2;
     private String field3;
 

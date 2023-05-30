167082c71f6d65f731b6af587634149fec79e792
Luigi Belanda <luigi.belanda.milani@gmail.com>
2023-05-30 13:44:42 -0300

    Step 19 - Exploring Internationalization for REST API

--- restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -31,13 +31,24 @@ formatada que inclui o valor fornecido na variável `name` na URL.
 
 package com.rest.webservices.restfulwebservices.helloworld;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContext;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 // REST API
 @RestController
 public class HelloWorldController {
+    private MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
     // hello-world
     @GetMapping(path = "/hello-world")
     public String helloWorld() {
@@ -52,5 +63,33 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world/path-variable/{name}")
     public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
         return new HelloWorldBean(String.format("Hello World, %s", name));
+    }
+
+    /*
+    O método `helloWorldInternationalized()` é um endpoint GET que retorna uma mensagem internacionalizada.
+
+    Vamos analisar o código passo a passo:
+
+    1. `Locale locale = LocaleContextHolder.getLocale();`: A classe `LocaleContextHolder` é
+    usada para obter o `Locale` atual da solicitação. O `Locale` representa a localização
+    ou idioma preferido do cliente que fez a solicitação.
+
+    2. `messageSource.getMessage("good.morning.message", null, "Default message", locale)`: O
+    `messageSource` é um bean do Spring Framework que gerencia as mensagens internacionalizadas.
+    O método `getMessage()` é usado para obter a mensagem internacionalizada com base na chave fornecida.
+
+       - O primeiro parâmetro `"good.morning.message"` é a chave da mensagem, que é usada para identificar a mensagem no arquivo de propriedades de mensagens internacionalizadas.
+       - O segundo parâmetro `null` é um array de argumentos opcionais que podem ser usados para substituir valores dinâmicos na mensagem.
+       - O terceiro parâmetro `"Default message"` é uma mensagem padrão que será retornada se a chave da mensagem não for encontrada.
+       - O último parâmetro `locale` indica o `Locale` a ser usado para recuperar a mensagem correta para o idioma preferido do cliente.
+
+       Em resumo, esse código obtém o `Locale` atual da solicitação e usa o `messageSource`
+       para obter a mensagem internacionalizada com base na chave fornecida. A mensagem
+       é retornada como resposta HTTP para o cliente que fez a solicitação.
+    */
+    @GetMapping(path = "/hello-world-internationalized")
+    public String helloWorldInternationalized() {
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message", null, "Default message", locale);
     }
 }
